d84da7a374bf7e0278a7d95c98dc88fd5fa391d2
TomekM <tomasz_matuszewski@o2.pl>
2020-05-07 23:16:32 +0200

    next steps django

--- filmyweb/admin.py
+++ filmyweb/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from filmyweb.models import Film
 
-# Register your models here.
+#admin.site.register(Film)
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    #fields = ["title", "description", "year"]
+    #exclude = ['description']
+    list_display = ['title', 'imdb_rating', 'year']
+    list_filter = ('year', 'imdb_rating')
+    search_fields = ('title', 'description')
--- filmyweb/models.py
+++ filmyweb/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    title = models.CharField(max_length=64, blank=False, unique=True)
+    year = models.PositiveIntegerField(default=2000)
+    description = models.TextField(default="")
+    premiere = models.DateField(null=True, blank=True)
+    imdb_rating = models.DecimalField(max_digits=4, decimal_places=2,
+                                      null=True, blank=True)
+    poster = models.ImageField(upload_to="posters", null=True, blank=True)
+
+    def __str__(self):
+        return self.title_with_year()
+
+    def title_with_year(self):
+        return f'{self.title} {str(self.year)}'
+
+
+
+
+
+
+
+
+
+
